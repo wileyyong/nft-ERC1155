97a5a355b1ea8d370c8146fb09868024b153bb33
razona <razonasistemas@gmail.com>
2021-11-27 15:55:52 +0100

    deployed to matic mainnet

--- truffle-config.js
+++ truffle-config.js
@@ -75,9 +75,9 @@ module.exports = {
       }),
       network_id: 3,       // Ropsten's id
       //  gas: 8000000,        // Ropsten has a lower block limit than mainnet
-    //  confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-     // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //  skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
+      //  confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      //  skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
       gasPrice: web3.utils.toWei('110', 'gwei'),
     },
     mainnet: {
@@ -100,12 +100,17 @@ module.exports = {
       ),
       network_id: 80001,
     },
+
     maticmainnet: {
       provider: function () {
         return new HDWalletProvider(mnemonic, 'https://rpc-mainnet.maticvigil.com/v1/153a2a693678a63ca32c29a331112f3247b5a72a');
       },
       network_id: '137',
+      gasPrice: web3.utils.toWei('100', 'gwei'),
+      skipDryRun: true,
+      gas: 6721975, 
     },
+
     bscTestnet: {
       provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
       network_id: 97,
